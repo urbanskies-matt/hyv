45a68987e6eeac762cc0ddc3f97772ed818dafb5
matt <matt@urbanskies.net>
2012-09-30 20:45:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hallyuverse::Application.config.secret_token = '0cf4a14c25e4f61c34139cbce2a9a7169bb15f4ecf925f5c9b725bfc70ec82a9fdfbb0c3af01ff4e82cbd66ca4ff3da07623c4e9f583d2c8d00ecaae6a329fe9'
